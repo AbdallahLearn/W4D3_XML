bf9e4c129d3a9d49d5ac06366bb8eefc4495331c
AbdallahLearn <abdallahjhn1@gmail.com>
2025-03-13 10:53:00 +0300

    adding visibility for all layouts

--- app/src/main/java/com/example/w4d3_xml/MainActivity.kt
+++ app/src/main/java/com/example/w4d3_xml/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.w4d3_xml
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.widget.EditText
 import androidx.activity.ComponentActivity
 
 import androidx.activity.enableEdgeToEdge
@@ -10,9 +14,30 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        setContentView(R.layout.constraint_layout)
+
+        val email= findViewById<EditText>(R.id.emailEditText)
+        email.addTextChangedListener(object:TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+//                Log.i("MainActvity ", "Char = $s , start = $start, before = $after, count = $count ")
+
+            }
+
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+//               Log.i("MainActvity ", "Char = $s , start = $start, before = $before, count = $count ")
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                Log.i("MainActvity ", "Editable = $s ")
+
+            }
+        })
+
+
 //        setContentView(R.layout.activty_main)
-        setContentView(R.layout.relative_layout)
-//        setContentView(R.layout.constraint_layout)
+//        setContentView(R.layout.relative_layout)
+
     }
 }
 
